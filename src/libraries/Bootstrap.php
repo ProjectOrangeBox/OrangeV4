cc7d388ba45e408ab3df5334413d71d8e36337c4
Don Myers <donmyers@me.com>
2019-09-26 11:55:33 -0400

    simplfiy index.php

--- src/libraries/Bootstrap.php
+++ src/libraries/Bootstrap.php
@@ -2,6 +2,9 @@
 
 define('NOVALUE','#PLEASE_THROW_AN_EXCEPTION#');
 
+/* ogrinal CI index.php */
+require 'bootstrap/Index.php';
+
 /* load the users bootstrap file if present */
 if (file_exists(APPPATH.'Bootstrap.php')) {
   require APPPATH.'Bootstrap.php';
