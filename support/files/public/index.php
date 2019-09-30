430a1f77d6f08e2175f144801306eeb3c7b7ffe8
Don Myers <donmyers@me.com>
2019-09-30 10:27:01 -0400

    updates

--- support/files/public/index.php
+++ support/files/public/index.php
@@ -3,12 +3,14 @@
 /* Where is this application located? */
 define('__ROOT__', realpath(__DIR__));
 
+/* Where is orange located? we need this to bootstrap the system. */
 define('ORANGEPATH',__ROOT__.'/vendor/projectorangebox/orange-v4');
 
-/* The name of THIS file */
+/* What is the name of THIS file */
 define('SELF',pathinfo(__FILE__, PATHINFO_BASENAME));
 
-/* This folder */
+/* Where is the WWW folder (ie usually this folder) */
 define('WWW',dirname(__FILE__));
 
+/* bootstrap */
 require ORANGEPATH.'/src/libraries/Bootstrap.php';
